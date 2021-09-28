9dfc2543e45f19ed218d97dad7f90c6ebaf02eb1
Nathan Lloyd <nathanalloyd@gmail.com>
2021-09-28 17:58:31 -0400

    Adds attack menu options.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,26 @@ namespace slime_attack
     {
         static void Main(string[] args)
         {
+            int desiredWidth = 110;
+            int desiredHeight = 55;
+            try
+            {
+                WindowHeight = desiredHeight;
+                WindowWidth = desiredWidth;
+            }
+            catch (System.ArgumentOutOfRangeException error)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("");
+                WriteLine("\t -- Warning -- ");
+                WriteLine("\t Your screen isn't big enough to match the game's desired width & height.");
+                WriteLine("\t Things may not look right unless you manually adjust the size of your console window.");
+                WriteLine("\t Press any key to continue.");
+                ReadKey(true);
+                ForegroundColor = ConsoleColor.Green;
+                Clear();
+            }
+
             Game myGame = new Game();
             myGame.Start();
         }
