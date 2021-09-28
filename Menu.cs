0a464995faa15da6b70ff0972ff058deda6621bc
Nathan Lloyd <nathanalloyd@gmail.com>
2021-09-28 16:27:10 -0400

    Adds title and main menu.

---
+++ Menu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace slime_attack
+{
+    class Menu
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+        }
+
+        private void DisplayOptions()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string sufix;
+                if (i == SelectedIndex)
+                {
+                    sufix = "<";
+                    ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    sufix = "";
+                    ForegroundColor = ConsoleColor.Green;
+                }
+
+                WriteLine($"{currentOption} {sufix}");
+            }
+            ForegroundColor = ConsoleColor.Green;
+            CursorVisible = false;
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayOptions();
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
